5e2e26a9071b144a244ea8db985de3ed0c96c338
Keegan McRoberts <keegan.mcroberts@gmail.com>
2022-09-27 12:22:52 -0500

    page banner and links to personal pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { SocialIcon } from 'react-social-icons';
+import ReactDOM from 'react-dom';
+// ReactDOM.render(<SocialIcon url="https://twitter.com/jaketrent" />);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header-name">
+        <a>Keegan McRoberts</a>
+        </div>
+        <div className="header-list">
+          <ul>
+            <a>Projects</a>
+          </ul>
+          <ul>
+            <a>About</a>
+          </ul>
+          <ul>
+            <a>Contact</a>
+          </ul>
+          </div>
+        <div className="header-icons">
+          <ul className="header-icons">
+            <SocialIcon url="https://www.linkedin.com/in/keegan-mcroberts/"/>
+            <SocialIcon url="https://github.com/keeganmcroberts"/>
+            <SocialIcon url="https://dev.to/keeganmcroberts"/>
+          </ul>
+        </div>
       </header>
+      <br></br>
+      <body className="app-body">
+        <h2>Contents</h2>
+      </body>
     </div>
   );
 }
