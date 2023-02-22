56298421960ab0cc50980496ee7c8da8727bfefe
Keegan McRoberts <keegan.mcroberts@gmail.com>
2023-02-22 15:05:26 -0600

    background and padding on banner links somehow messed up, had to fix them

--- src/components/nationalPark.js
+++ src/components/nationalPark.js
@@ -5,13 +5,13 @@ function Park(){
         <div className="project-card">
               <img src={park}></img>
               <h2 className="projectName">National Park Locator</h2>
-              <h4 className="project-description">A new take on everybody’s favorite family-friendly card game, Uno. Um is a mock demonstration of the logic necessary for the card game.</h4>
+              <h4 className="project-description">A user-personalized and interactive National Park website intended for user research, cataloguing experiences, and reviewing different National Parks. (Demo coming soon!)  </h4>
               <h5 className="software">Stack:</h5>
-              <h5 className="softwares">MapBoxGL ReactJS Rails API</h5>
+              <h5 className="softwares">MapBoxGL React Rails API</h5>
               <br></br>
               <div className="buttons">
-                <a className="reference-link1" href="https://github.com/keeganmcroberts/Rails-React-Uno" target="_blank">Code</a>
-                <a className="reference-link2" href="https://www.loom.com/share/4b7d52ff4ef54457bd38cca5b83f35fa" target="_blank">Demo</a>
+                <a className="reference-link1" href="https://github.com/keeganmcroberts/Natioanl-Park-Locator" target="_blank">Code</a>
+                <a className="reference-link2" target="_blank">Demo</a>
               </div>
         </div>
     )
